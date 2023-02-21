d64b87084c6fefdaad13cfb83d3aa5df8a98f622
Isabella1807 <megaoreomuffin@hotmail.com>
2023-02-21 20:32:35 +0100

    show and clear error message

--- loginFunction.js
+++ loginFunction.js
@@ -13,5 +13,24 @@ function submitLoginForm(){
     const userPassword = document.getElementById("userPassword").value;
     const rememberMe = document.getElementById("rememberMe").checked;
 
+    if(userMail === "" || userPassword === "") {
+        setErrorMessage("Felter ikke udfyldt");
+    }
+    else{
+        console.log("felter er ikke tom");
+        setErrorMessage("");
+    }
+
     console.log(rememberMe);
 }
+
+//error message recipe
+function setErrorMessage(errorMessage){
+    if(errorMessage === "") {
+        document.getElementById("errorMessageContainer").innerHTML = "";
+    } else {
+        document.getElementById("errorMessageContainer").innerHTML = "<h1>Der opstod en fejl</h1><p>" + errorMessage + "</p>";
+    }
+}
+
+
