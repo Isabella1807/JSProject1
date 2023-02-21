b31cfdba49396722f6cc7e81e2b105306599ac7a
Isabella1807 <megaoreomuffin@hotmail.com>
2023-02-21 14:51:08 +0100

    html for form container

--- loginFunction.js
+++ loginFunction.js
@@ -1,20 +1,3 @@
-// const users{
-//     mails
-//     passwords
-// }
-//
-//
-// document.getElementById("loginButton").onclick = function(){
-//
-//     slide in the login div
-// }
-
-// $(document).ready(function(){
-//     $("loginButton").click(function(){
-//         $("box").animate({bottom: '250px'});
-//     });
-// });
-
 const loginContainer = document.getElementById("loginContainer");
 
 function showLoginContainer(){
