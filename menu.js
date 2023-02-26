df329eaaf81d780996467951f7f69ab35dd25d5a
Reva42 <62376593+Reva42@users.noreply.github.com>
2023-02-26 22:45:35 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,9 +1,38 @@
 // JavaScript Document
+
+const imageWrapper = document.querySelector('.image-wrapper')
+const imageItems = document.querySelectorAll('.image-wrapper > *')
+const imageLength = imageItems.length
+const perView = 3
+let totalScroll = 0
+const delay = 2000
+
+imageWrapper.style.setProperty('--per-view', perView)
+for(let i = 0; i < perView; i++) {
+  imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML)
+}
+
+let autoScroll = setInterval(scrolling, delay)
+
+function scrolling() {
+  totalScroll++
+  if(totalScroll == imageLength + 1) {
+    clearInterval(autoScroll)
+    totalScroll = 1
+    imageWrapper.style.transition = '0s'
+    imageWrapper.style.left = '0'
+    autoScroll = setInterval(scrolling, delay)
+  }
+  const widthEl = document.querySelector('.image-wrapper > :first-child').offsetWidth + 24
+  imageWrapper.style.left = `-${totalScroll * widthEl}px`
+  imageWrapper.style.transition = '.3s'
+}
+
 function myFunction(imgs) {
   // Get the expanded image
-  var expandImg = document.getElementById("expandedImg");
+  let expandImg = document.getElementById("expandedImg");
   // Get the image text
-  var imgText = document.getElementById("imgtext");
+  let imgText = document.getElementById("imgtext");
   // Use the same src in the expanded image as the image being clicked on from the grid
   expandImg.src = imgs.src;
   // Use the value of the alt attribute of the clickable image as text inside the expanded image
